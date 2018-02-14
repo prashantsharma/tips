0d5be2c0300b70c26d4fc122abeb46f770bd8cdc
Prashant Sharma <prashant.pab@gmail.com>
2018-02-14 20:59:51 +0530

    Array loop of & in

--- javascript/array-tips.js
+++ javascript/array-tips.js
@@ -1,4 +1,15 @@
 //Check if array or not
 
-var arr = [1, 2, 3];
+let arr = ["a", "b", "c", "d"];
 console.log(arr instanceof Array);
+
+//Loop array in ES6
+for (let item of arr) {
+  console.log(item);
+}
+
+//Loop array in before ES6
+for (let idx in arr) {
+  console.log(idx);
+  console.log(arr[idx]);
+}
