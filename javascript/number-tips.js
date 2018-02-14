046e56be3bf2abd93d0680f6c9a4ee6f2a0b7029
Prashant Sharma <prashant.pab@gmail.com>
2018-02-14 21:26:58 +0530

    ES6 NUMBERS

---
+++ javascript/number-tips.js
@@ -0,0 +1,8 @@
+//Numbers in ES6 - Every line will print 29
+console.log(Number(29));
+console.log(Number(035));
+console.log(Number(0o35));
+console.log(Number(0x1d));
+console.log(Number(0b11101));
+
+
